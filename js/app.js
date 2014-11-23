a0257aa8f9ba0c8ef491b35fa1218fccef103d87
Phyks <webmaster@phyks.me>
2014-11-23 15:25:10 +0100

    Prepare new design integration

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 "use strict"; // see strict mode
 
-var config = (function () {
+var Config = (function () {
     var tiles_provider = 'http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg';
     var api_key = '5eec7c1a3babb6b2abeabb0143c635d2d9aff1c3';
     var stations_base_url = 'https://api.jcdecaux.com/vls/v1/stations?contract=paris&apiKey=';
@@ -9,7 +9,8 @@ var config = (function () {
     return {
         tiles_provider: tiles_provider,
         stations_url: stations_base_url + api_key,
-        realtime_url: realtime_url + api_key
+        realtime_url: realtime_url + api_key,
+        max_starred_stations: 10
     }
 })();
 
@@ -21,20 +22,11 @@ if (typeof(Array.prototype.last) != 'function') {
 };
 
 // Bind a startswith method on String
-if (typeof( String.prototype.startsWith) != 'function') {
-    String.prototype.startsWith = function( str ) {
-      return this.substring( 0, str.length ) === str;
+if (typeof(String.prototype.startsWith) != 'function') {
+    String.prototype.startsWith = function(str) {
+      return this.substring(0, str.length) === str;
     }
 };
-
-/**************
- * Global vars
- **************/
-
-var stations = [];  // Store only the relevant stations
-
-var map = false;  // Handles minimaps
-var slides_container = false;  // Global object for the Slides API
 
 /***************
  * Geolocation *
@@ -96,10 +88,16 @@ var Geolocation = (function () {
         }
     };
 
+    // Returns the current position
+    var getPosition = function () {
+        return coords;
+    }
+
     return {
         init: init,
         waitPosition: waitPosition,
-        noWaitPosition: noWaitPosition
+        noWaitPosition: noWaitPosition,
+        getPosition: getPosition
     };
 })();
 
@@ -110,19 +108,16 @@ var Geolocation = (function () {
 
 var Stations = (function() {
     var full_stations_list = null;
+    var ordered_stations_list = null;
+    var starred_stations_list = null;
     var timer = null;
-
-    // Get the full stations list
-    var getFullList = function () {
-        return full_stations_list;
-    };
 
     // Refresh the JSON of the stations at JCDecaux OpenData API and stores it in localStorage.
     var refresh = function () {
         // Reset stations state to null, while loading the ressource
         full_stations_list = null;
         // Update the stations list
-        $.getJSON(window.config['stations_url'], function (data, status, jqXHR) {
+        $.getJSON(window.Config['stations_url'], function (data, status, jqXHR) {
             localStorage.setItem('last_stations_update', Date.now());
             localStorage.setItem('stations', jqXHR.responseText);
             full_stations_list = data;
@@ -135,6 +130,12 @@ var Stations = (function() {
             full_stations_list = JSON.parse(localStorage.getItem('stations'));
         } catch (e) {
             full_stations_list = null;
+        }
+
+        try {
+            starred_stations_list = JSON.parse(localStorage.getItem('starred_stations'));
+        } catch (e) {
+            starred_stations_list = [];
         }
 
         // Update stations list once per month
@@ -163,12 +164,116 @@ var Stations = (function() {
         }
     };
 
+    // Converts an angle from degrees to rads
+    var deg2rad = function (angle) {
+        return angle * Math.PI / 180;
+    };
+
+    // Computes the distance between two points identified by latitude / longitude
+    var distance = function (coords, station) {
+        var latitude1 = deg2rad(coords.latitude);
+        var longitude1 = deg2rad(coords.longitude);
+        var latitude2 = deg2rad(station.position.lat);
+        var longitude2 = deg2rad(station.position.lng);
+        var a = Math.pow(Math.sin(latitude2 - latitude1)/2, 2) + Math.cos(latitude1) * Math.cos(latitude2)*Math.pow(Math.sin(longitude2 - longitude1)/2, 2);
+        var c = 2*Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        var R = 6371000;
+        var distance = R*c;
+        return distance;
+    };
+
+    // Computes distances for each stations
+    var computeDistances = function (list, coords) {
+        list = full_stations_list.map(function (item) {
+            item["distance"] = distance(coords, item);
+            return item;
+        });
+        return list;
+    };
+
+    // Returns the stations ordered by distance to the position identified by coords
+    var orderByDistance = function (coords) {
+        ordered_stations_list = computeDistances(full_stations_list, coords);
+        ordered_stations_list.sort(function (a, b) {
+            return a.distance - b.distance;
+        });
+        return ordered_stations_list;
+    };
+
+    // Fetch latest infos for the specified stations
+    var fetchStations = function (stations) {
+        // TODO
+    };
+
+    // Returns `limit` closest stations with up to date infos and a matching criterion
+    var getClosestStations = function (limit, filter) {
+        if (typeof(limit) == 'undefined') {
+            limit = -1;
+        }
+        else if (typeof(limit) == 'function' && typeof(filter) != 'function') {
+            filter = limit;
+            limit = -1;
+        }
+        else {
+            limit = parsInt(limit);
+        }
+
+        if (limit === NaN) {
+            limit = -1;
+        }
+        if (typeof(filter) == "undefined") {
+            filter = function (item) { return true; };
+        }
+
+        // TODO
+    };
+
+    // Star / Unstar a station
+    var toggleStarStation = function (station_id) {
+        var index = starred_stations.indexOf(station_id);
+        if (index != -1) {
+            starred_stations.splice(index, 1);
+        }
+        else {
+            if (starred_stations.length >= window.Config.max_starred_stations) {
+                return false;
+            }
+            starred_stations.push(station_id);
+        }
+        localStorage.setItem('starred_stations', JSON.stringify(starred_stations));
+        return true;
+    };
+
+    // Retrieve the up to date list of starred stations
+    var getStarredStations = function (coords) {
+        var full_starred_stations_list = [];
+        for (var i = 0; i < starred_stations.length; i++) {
+            full_starred_stations_list.push($.grep(full_stations_list.grep, function (item) { return item.id == station_id; }));
+        }
+        full_starred_stations_list = computeDistance(full_starred_stations, coords);
+        // TODO : Update
+        return full_starred_stations_list;
+    };
+
+    // Returns the full list of stations, ordered by distance if available
+    var getFullList = function () {
+        if (ordered_stations_list !== null) {
+            return ordered_stations_list;
+        }
+        else {
+            return full_stations_list;
+        }
+    };
+
     return {
-        getFullList: getFullList,
         init: init,
         refresh: refresh,
         waitList: waitList,
-        noWaitList: noWaitList
+        noWaitList: noWaitList,
+        getClosestStations: getClosestStations,
+        toggleStarStation: toggleStarStation,
+        getStarredStations: getStarredStations,
+        getFullList: getFullList
     };
 })();
 
@@ -181,29 +286,32 @@ var Views = (function () {
     var index = function () {
         Geolocation.noWaitPosition();
         Stations.noWaitList();
-        $('.station-info').html('<div class="entry bikes"><span>vélos<br/>disponibles</span><img class="entry--logo" alt="" src="img/velib.svg" /></div><div class="entry stands"><span>places<br/>libres</span><img class="entry--logo" alt="" src="img/borne.svg" /></div><div class="entry stared"><span>Favoris</span><img class="entry--logo" alt="" src="img/favori.svg" /></div><div class="entry search"><span>Rechercher</span><img class="entry--logo" alt="" src="img/loupe.svg" /></div>');
+        $('.station-info').html('<div class="entry bikes"><span>vélos<br/>disponibles</span><img class="entry--logo" alt="" src="img/velib.svg" /></div><div class="entry stands"><span>places<br/>libres</span><img class="entry--logo" alt="" src="img/borne.svg" /></div><div class="entry starred"><span>Favoris</span><img class="entry--logo" alt="" src="img/favori.svg" /></div><div class="entry search"><span>Rechercher</span><img class="entry--logo" alt="" src="img/loupe.svg" /></div>');
 
         $('.entry.bikes').click(function () { window.location.hash = "/bikes"; });
         $('.entry.stands').click(function () { window.location.hash = "/stands"; });
-        $('.entry.stared').click(function () { window.location.hash = "/stared"; });
+        $('.entry.starred').click(function () { window.location.hash = "/starred"; });
         $('.entry.search').click(function () { window.location.hash = "/search"; });
     };
 
     var bikes = function () {
         if (Geolocation.waitPosition(bikes) && Stations.waitList(bikes)) {
+            console.log(Stations.getClosestStations(Geolocation.getPosition(), 10, function () { return item.available_bikes > 0 }));
             $('.station-info').html('');
         }
     };
 
     var stands = function () {
         if (Geolocation.waitPosition(stands) && Stations.waitList(stands)) {
+            console.log(Stations.getClosestStations(Geolocation.getPosition(), 10, function () { return item.available_bike_stands > 0 }));
             $('.station-info').html('');
         }
     };
 
-    var stared = function () {
+    var starred = function () {
         Geolocation.noWaitPosition();
-        if (Stations.waitList(stared)) {
+        if (Stations.waitList(starred)) {
+            console.log(Stations.getClosestStations(Geolocation.getPosition(), 10, function () { return item.starred > 0 }));
             $('.station-info').html('');
         }
     };
@@ -219,7 +327,7 @@ var Views = (function () {
         index: index,
         bikes: bikes,
         stands: stands,
-        stared: stared,
+        starred: starred,
         search: search
     }
 })();
@@ -237,8 +345,8 @@ var Routing = (function () {
         else if (hash.startsWith("/stands")) {
             Views.stands();
         }
-        else if (hash.startsWith("/stared")) {
-            Views.stared();
+        else if (hash.startsWith("/starred")) {
+            Views.starred();
         }
         else if (hash.startsWith("/search")) {
             Views.search();
