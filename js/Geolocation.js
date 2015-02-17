4c69a6671339ce669b4c7d7465940f6e550b58fb
Élie Michel <elie.michel@ens.fr>
2015-02-17 10:42:09 +0100

    Clean up Geolocation

--- js/Geolocation.js
+++ js/Geolocation.js
@@ -14,17 +14,8 @@ var Geolocation = (function() {
     };
 
     var errorFunction = function(error) {
+        coords = null;
         switch (error.code) {
-            case error.TIMEOUT:
-                $('.info--content').addClass('error').html("Timeout.");
-                //Restart with a greater timeout
-                navigator.geolocation.getCurrentPosition(positionSuccessFunction, positionErrorFunction, {
-                    enableHighAccuracy: true,
-                    maximumAge: 60000,
-                    timeout: 5000
-                });
-                break;
-
             case error.PERMISSION_DENIED:
                 $('.info--content').addClass('error').html("Accès refusé.");
                 break;
@@ -40,9 +31,7 @@ var Geolocation = (function() {
         if (navigator.geolocation) {
             navigator.geolocation.watchPosition(successFunction, errorFunction, {
                 enableHighAccuracy: true,
-                maximumAge: 10000,
-                timeout: 60000,
-                frequency: 10000
+                maximumAge: 10000
             });
         } else {
             $('.info--content').addClass('error').html("Votre navigateur ne supporte pas l'API de géolocalisation.");
